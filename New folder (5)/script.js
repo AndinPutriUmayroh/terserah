11241cf219308c9e6872c8c59a059205a02170ff
AndinPutriUmayroh <andinputriumayroh@gmail.com>
2024-12-04 12:11:55 +0700

    Add files via upload

---
+++ New folder (5)/script.js
@@ -0,0 +1,7 @@
+script.js
+// Fungsi untuk menampilkan pesan
+function showMessage() {
+    const messageElement = document.getElementById('message');
+    messageElement.innerText = "Halo! Ini adalah pesan yang ditampilkan oleh JavaScript.";
+    messageElement.style.color = "#28a745";
+}
